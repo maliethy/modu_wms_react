fc62bf9090f5aa881727ae7ae8e236c3a97a6b39
maliehty <maliethy@gmail.com>
2022-01-30 19:59:38 +0900

    feat: add table prefix for responsive design, make Footerbar responsive  with tablet size

--- Components/Footerbar.tsx
+++ Components/Footerbar.tsx
@@ -2,30 +2,30 @@ import react from 'react';
 
 function Footerbar() {
   return (
-    <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
-      <ul className="flex items-center flex-wrap mb-6 md:mb-0">
+    <footer className="bg-white tb:bg-red-50 flex flex-col justify-center items-center tb:flex-row  tb:justify-between shadow rounded-lg p-4 md:p-6 md:p-8 my-6 mx-4">
+      <ul className="flex items-center flex-wrap mb-6 tb:mb-0">
         <li>
-          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6">
+          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 tb:mr-6">
             회사소개
           </a>
         </li>
         <li>
-          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6">
+          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 tb:mr-6">
             이용약관
           </a>
         </li>
         <li>
-          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6">
+          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 tb:mr-6">
             개인정보 처리방침
           </a>
         </li>
         <li>
-          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 md:mr-6">
+          <a href="#" className="text-sm font-normal text-gray-500 hover:underline mr-4 tb:mr-6">
             고객센터
           </a>
         </li>
       </ul>
-      <div className="flex sm:justify-center space-x-6">
+      <div className="flex items-center">
         <a href="https://oms.modument.co.kr/" className="text-gray-500 hover:text-gray-900">
           OMS 바로가기
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      //Responsive Design
+      tb: '600px', //tablet
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
